c094a8d632e1e0fc8a73ab57a4b2e0b79b741a56
aspatil0 <aspatil2904@gmail.com>
2025-09-20 21:48:41 +0530

    day1- added variables

---
+++ FOUNDATION/variables.py
@@ -0,0 +1,28 @@
+# DAY 1 
+
+# LEARNING THE VARIBALE  | PYTHON BASICS
+
+
+a= 10
+# no need to declare the variable type
+# python is a dynamically typed language
+
+print(a)
+
+b= 20.5
+c="Adityaraj Patil"
+d= True
+
+
+#rules to declare a variable
+# 1. should start with a letter or underscore
+# 2. can contain letters, numbers and underscores
+# 3. case sensitive
+# 4. cannot use reserved keywords
+# 5. should not start with a number
+# 6. cannot contain special characters
+# 7. cannot use spaces
+
+
+#9a= 10  # invalid
+a9= 10   # valid
