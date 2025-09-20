957e7297124a765da13415d543389c5322674bf3
aspatil0 <aspatil2904@gmail.com>
2025-09-20 21:58:12 +0530

    day1- added dtypes use

---
+++ FOUNDATION/datatypes.py
@@ -0,0 +1,21 @@
+# same like other programming language here we see datataypes
+
+a= "Aditya"
+print(type(a))
+
+b=10
+print(type(b))
+
+c=5.65
+print(type(c))
+
+d=True
+print(type(d))
+
+#e= float(a)
+#print(e)
+
+
+x="2"
+f=float(x)
+print(x)
