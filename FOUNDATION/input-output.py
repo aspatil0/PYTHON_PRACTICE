2afdabde48d62a5fe59a9a119caf3513e959f387
aspatil0 <aspatil2904@gmail.com>
2025-09-21 21:34:43 +0530

    day2- added dtypes use

---
+++ FOUNDATION/input-output.py
@@ -0,0 +1,19 @@
+
+#To get data as input in python
+
+
+name = input("enter your name")
+print(name)
+
+a = int(input("enter num1"))
+b = int(input("enter num 2"))
+
+print(a+b)
+
+a = "aditya"
+print(a)
+
+
+cc= float(input("conversion"))
+dd= int(cc)
+print(dd)
