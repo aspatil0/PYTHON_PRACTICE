05761026d543e79c5828aad2ebed5da89234882d
aspatil0 <aspatil2904@gmail.com>
2025-09-21 21:38:27 +0530

    day2- added oprator use

---
+++ FOUNDATION/operators.py
@@ -0,0 +1,28 @@
+#operators in python
+
+a = 10
+b = 20
+
+#arithmetic operators
+c=b
+print(c)
+print(a+b)
+print(a-b)
+print(a*b)
+print(b/a)  #float division
+print(b%a)  #modulus
+print(b//a)     #floor division
+print(b**a) #exponent
+print(a**b)     
+print(a**2)
+
+#comparison operators
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+
+#logical operators
+print(a>50 and b>15)
+print(a>5 or b<15)
+print(not(a>b))
