db41bbf69d9af66ad21e3b5ffe961ece86616d21
aspatil0 <aspatil2904@gmail.com>
2025-09-22 14:19:11 +0530

    day3- added learning patern building

---
+++ PATTERNS/STARS/RightTrianglePattern.py
@@ -0,0 +1,6 @@
+n = int(input("enter number"))
+
+for i in range(1,n+1):
+    for  j in range (i):
+        print("X", end="")
+    print()
---
+++ PATTERNS/STARS/inverserightandgle.py
@@ -0,0 +1,6 @@
+n = int(input("enter your count"))
+
+for i in range (n,0,-1):
+    for j in range (i):
+        print("x",end="")
+    print()
