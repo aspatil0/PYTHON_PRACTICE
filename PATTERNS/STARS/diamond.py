599ff2d221ea7e1d42f0ea868a1a1859e3a84497
aspatil0 <aspatil2904@gmail.com>
2025-09-24 23:02:53 +0530

    day4- added learning patern building

--- PATTERNS/STARS/diamond.py
+++ PATTERNS/STARS/diamond.py
@@ -4,3 +4,13 @@ for i in range(1,n+1):
     print(" "*(n-i),"*"*(2*i-1))
 for i in range(n-1,0,-1):
     print(" "*(n-i),"*"*(2*i-1))
+
+
+    #using i an j
+n=5 
+for i in range(1,n+1):
+    for j in range(1,n-i+1):
+        print(" ",end="")
+    for j in range(1,2*i):
+        print("*",end="")
+    print()
