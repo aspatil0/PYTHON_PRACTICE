42315d503126b694b60707d607d9058ab99fdd3b
aspatil0 <aspatil2904@gmail.com>
2025-09-27 16:35:39 +0530

    day- added learning casting

---
+++ DATA STRUCTURE/Explicit.py
@@ -0,0 +1,7 @@
+x = "100"    # string
+y = int(x)   # string → int
+z = float(y) # int → float
+
+print(x, type(x))  # "100" <class 'str'>
+print(y, type(y))  # 100 <class 'int'>
+print(z, type(z))  # 100.0 <class 'float'>
